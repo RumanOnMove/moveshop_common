fa7c1b1071f8a42d2d47a57f5c6d181a060772d5
RumanOnMove <oruman@moveon.com.bd>
2023-05-23 16:15:51 +0600

    Client Feature tested and updated

--- app/controllers/moveshop_common/clients_controller.rb
+++ app/controllers/moveshop_common/clients_controller.rb
@@ -1,28 +1,35 @@
 module MoveshopCommon
   class ClientsController < ApplicationController
+    skip_before_action :verify_authenticity_token, only: [:create, :update, :destroy]
     def index
-      clients = Spree::OauthApplication.all
+      clients = Client.all
       render json: { data: clients }, status: :ok
     end
 
     def show
-      client = Spree::OauthApplication.find_by(id: params[:id])
+      client = Client.find_by(id: params[:id])
 
       if client
         render json: { data: client, message: 'Client retrieved successfully' }, status: :ok
+      else
+        render json: { message: 'Client not found' }, status: :not_found
       end
     end
 
     def create
-      client = Spree::OauthApplication.new(client_params)
+      client = Client.new(client_params)
 
       if client.save
         render json: { data: client, message: 'Client created successfully' }, status: :created
+      else
+        render json: { error: client.errors }, status: :unprocessable_entity
       end
+    rescue StandardError => e
+      render json: { error: e.message }
     end
 
     def update
-      client = Spree::OauthApplication.find_by(id: params[:id])
+      client = Client.find_by(id: params[:id])
 
       if client.update(client_params)
         render json: { data: client, message: 'Client updated successfully' }, status: :ok
@@ -34,7 +41,7 @@ module MoveshopCommon
     end
 
     def destroy
-      client = Spree::OauthApplication.find_by(id: params[:id])
+      client = Client.find_by(id: params[:id])
 
       if client&.destroy
         render json: { message: 'Client deleted successfully' }, status: :ok
@@ -46,7 +53,7 @@ module MoveshopCommon
     private
 
     def client_params
-      params.permit(:name, :scopes)
+      params.require(:client).permit(:name, :scopes)
     end
   end
 end
--- app/models/moveshop_common/client.rb
+++ app/models/moveshop_common/client.rb
@@ -1,4 +1,20 @@
 module MoveshopCommon
   class Client < Spree::OauthApplication
+    validates :name, presence: true, length: { minimum: 5, maximum: 25 }, uniqueness: true
+    validates :scopes, presence: true
+
+    def self.scopes
+      [
+        { name: 'read_products', description: 'Read products' },
+        { name: 'write_products', description: 'Write products' },
+        { name: 'read_orders', description: 'Read orders' },
+        { name: 'write_orders', description: 'Write orders' }
+      # Add more scopes as needed
+      ]
+    end
+
+    def scopes_enum
+      self.class.scopes.map { |scope| [scope[:description], scope[:name]] }
+    end
   end
 end
